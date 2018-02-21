5a4481335581ce22b0f8a2de3616dafbadc4e327
rafpyprog <rafael.alves.ribeiro@gmail.com>
2018-02-21 11:08:10 -0300

    Custom CSS for map-styles.ipynb

---
+++ examples/csscell.py
@@ -0,0 +1,11 @@
+from IPython.core.display import display, HTML
+
+display(HTML('''<style>#notebook-container {
+  webkit-box-shadow: none !important;
+  box-shadow: none !important;
+}
+
+div.output_subarea {
+  max-width: 100% !important;
+  padding: 0.1em 0em 0em 0em !important;
+}</style>'''))
